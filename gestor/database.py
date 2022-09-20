28b3ba6e8b5dfedde5d5e63b00f32494c55d30fa
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-09-20 16:03:43 +0200

    clase clientes

--- gestor/database.py
+++ gestor/database.py
@@ -5,3 +5,38 @@ class Cliente:
         self.apellido = apellido
     def __str__(self):
         return f"({self.dni}) {self.nombre} {self.apellido}"
+
+
+class Clientes:
+    #Lista de clientes
+    lista = []
+
+    @staticmethod
+    def buscar(dni):
+        for cliente in Clientes.lista:
+            if cliente.dni == dni:
+                return cliente
+
+    @staticmethod
+    def crear(dni, nombre, apellido):
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        Clientes.guardar()
+        return cliente
+
+    @staticmethod
+    def modificar(dni, nombre, apellido):
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[indice].nombre = nombre
+                Clientes.lista[indice].apellido = apellido
+                Clientes.guardar()
+                return Clientes.lista[indice]
+
+    @staticmethod
+    def borrar(dni):
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(indice)
+                Clientes.guardar()
+                return cliente
