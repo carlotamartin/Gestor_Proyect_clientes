d73af916b1d46310b941e5137518c1865b9d6b63
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-09-23 18:33:15 +0200

    guardar

--- gestor/test/test_database.py
+++ gestor/test/test_database.py
@@ -1,6 +1,7 @@
 import copy
 import unittest
 import databases as db
+import helpers
 
 
 class TestDatabase(unittest.TestCase):
@@ -36,6 +37,12 @@ class TestDatabase(unittest.TestCase):
         cliente_rebuscado = db.Clientes.buscar('48H')
         self.assertEqual(cliente_borrado.dni, '48H')
         self.assertIsNone(cliente_rebuscado)
+
+    def test_dni_valido(self):
+        self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('23223S', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))
 
 if __name__ == '__main__':
     unittest.main()
