b46c113b9d83366da6fe207027f018a6240d9dab
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-09-23 18:32:45 +0200

    creacion del csv y más cosas

--- gestor/menu.py
+++ gestor/menu.py
@@ -30,7 +30,13 @@ def iniciar():
 
         elif opcion == '3':
             print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+
+            dni = None
+            while True:
+                dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
+
             nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
             db.Clientes.crear(dni, nombre, apellido)
